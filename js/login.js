f18e7de4dcd42b5e5d2813371d8b56edc13df76c
Ahmed Khaled <ahmedharhash0048@gmail.com>
2026-01-24 17:29:59 +0200

    edit for swal in login

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 const API_BASE_URL = 'https://step-1-academy.runasp.net/api';
 document.addEventListener("DOMContentLoaded", () => {
     // API Configuration
